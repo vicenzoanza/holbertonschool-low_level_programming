e530bb859fea8f25128e316a22afc1a24b37edfd
vicenzoanza <viceanza2010@hotmail.com>
2023-02-16 05:10:44 -0800

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,10 @@ int intType;
 long longType;
 double doubleType;
 float floatType;
-printf("Size of a char: %zu byte\n", sizeof(charType));
-printf("Size of an int: %zu bytes\n", sizeof(intType));
-printf("Size of a long int: %zu bytes\n", sizeof(longType));
-printf("Size of a long long int: %zu bytes\n", sizeof(doubleType));
-printf("Size of a float: %zu bytes\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
