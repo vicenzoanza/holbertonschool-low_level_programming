f47a744afc0dfe1b968afc0e4e873111f741351e
vicenzoanza <viceanza2010@hotmail.com>
2023-02-17 04:52:14 -0800

    task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*C program to print all lower case alphabets.*/
+int main(void)
+{
+char i;
+printf("\n\nLower case characters:\n");
+for (i = 'a'; i <= 'z'; i++)
+printf("%c", i);
+return (0);
+}
