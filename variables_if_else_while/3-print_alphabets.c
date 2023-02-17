edd5e8332dd6ee2b4c80833ade2512214288c998
vicenzoanza <viceanza2010@hotmail.com>
2023-02-17 05:27:43 -0800

    task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 int main(void)
 {
 char i;
-for(i='a'; i<='z'; i++)
-printf("%c",i);
-for(i='A'; i<='Z'; i++)
-printf("%c",i);
+for (i = 'a'; i <= 'z'; i++)
+printf("%c", i);
+for (i = 'A'; i <= 'Z'; i++)
+printf("%c", i);
 printf("\n");
 return (0);
 }
