53ee44a3b71cd62d5121dd26947b0c9735259706
vicenzoanza <viceanza2010@hotmail.com>
2023-02-17 05:22:21 -0800

    task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -4,10 +4,9 @@ int main(void)
 {
 char i;
 for(i='a'; i<='z'; i++)
-printf("%c ",i);
-printf("\n");
+printf("%c",i);
 for(i='A'; i<='Z'; i++)
-printf("%c ",i);
+printf("%c",i);
 printf("\n");
 return (0);
 }
