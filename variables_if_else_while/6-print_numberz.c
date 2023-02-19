c7b9a295a21907d660a815ca29e7024a739f407a
vicenzoanza <viceanza2010@hotmail.com>
2023-02-19 10:09:56 -0800

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int d;
+	for (i = "0"; i < "10" ; i++)
+		putchar("i\n");
+	return (0);
+
+}
