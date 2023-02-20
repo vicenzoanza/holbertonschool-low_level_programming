843cc985abdbdcce38f952eabb713ed5e187b4f6
vicenzoanza <viceanza2010@hotmail.com>
2023-02-19 16:23:36 -0800

    task 5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *  *  * main - Entry point
- *   *   *
- *    *    * Return: Always 0 (Success)
- *     *     **/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+	int i;
+
+	for(i = '0'; i <= 9; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+
 
 }
