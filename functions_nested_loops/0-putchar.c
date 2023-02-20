a9d560776ab743310f2d268dbf3dced96a2ec784
vicenzoanza <viceanza2010@hotmail.com>
2023-02-19 18:49:04 -0800

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- *  * main - Prints _putchar, followed by a new line
- *   * Return: Always 0 (Success)
- *   */
+ * main - Prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ **/
 
 int main(void)
 {
 char string[9] = "_putchar";
 int i;
+
 for (i = 0; i <= 7; i++)
 {
 _putchar(string[i]);
