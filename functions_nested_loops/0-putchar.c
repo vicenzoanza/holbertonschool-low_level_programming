fad8975dc5196c0eb2317f8fc4681975261a5130
vicenzoanza <viceanza2010@hotmail.com>
2023-02-19 17:11:37 -0800

    task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/*
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+int _putchar;
+
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('c');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+
+
+
+
+
+}
