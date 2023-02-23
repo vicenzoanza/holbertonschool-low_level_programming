f7a7611c5b31390e2bf3e20b5c464151cc2d9f68
vicenzoanza <viceanza2010@hotmail.com>
2023-02-23 08:50:30 -0800

    task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- main - check the code.
- Return: Always 0.
+*main - check the code.
+*Return: Always 0.
 **/
 int _isupper(int c)
-{    
-
-	if ( c >= 65 && c <= 90 )
+{
+	if (c >= 65 && c <= 90)
 	return (1);
-	else 
+	else
 	return (0);
 
 }
