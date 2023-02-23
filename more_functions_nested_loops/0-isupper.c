eab18429fdcce2b62282fcfb7aca45fe1228ec09
vicenzoanza <viceanza2010@hotmail.com>
2023-02-23 08:46:30 -0800

    task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,15 @@
-#include <main.h>
+#include "main.h"
 #include <stdio.h>
-#include <ctype.h>
 /**
- * main - check the code.
- *
- * Return: Always 0.
- **/
-int main(void)
-{
-	char c;
-	int _isupper(int c);
+ main - check the code.
+ Return: Always 0.
+**/
+int _isupper(int c)
+{    
 
-if	 c = 'A'
-	 printf("%c: %d\n", c, _isupper(c));
-return (1);
-else 	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
+	if ( c >= 65 && c <= 90 )
+	return (1);
+	else 
 	return (0);
+
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _isupper(int c); /** 0 **/
-
-
-
-#endif
