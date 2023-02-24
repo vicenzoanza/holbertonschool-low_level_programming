94dabab0c4fef3e537ddfe1f71987ff966ca8e5e
vicenzoanza <viceanza2010@hotmail.com>
2023-02-24 11:20:50 -0800

    task 1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isdigit - checks for a digit
+* @c: the digit to check.
+* Return: 1 if is a digit, 0 otherwise.
+**/
+int _isdigit(int c)
+{
+
+if (c >= 48 && c <= 57)
+{
+	return (1);
+}
+return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 int _isupper(int c); /** 0 **/
+int _isdigit(int c); /** 1 **/
