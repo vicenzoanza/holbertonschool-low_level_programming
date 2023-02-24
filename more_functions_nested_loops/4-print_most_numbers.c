938a6b5f1b1c1cc7dba33250d5f7916e4753af91
vicenzoanza <viceanza2010@hotmail.com>
2023-02-24 14:38:00 -0800

    task 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,15 +2,16 @@
 /**
 * print_numbers - prints 0 to 9, dont print 2 and 4, followed by a new line.
 **/
-void print_numbers(void)
+void print_most_numbers(void)
 {
-int n = 48;
+int n;
 
-while (n <= 57)
+for (n = 48; n <= 57; n++)
+{
+if (n != 50 && n != 52)
 {
 _putchar(n);
-n++;
 }
-if (n != 50 && n != 52)
+}
 _putchar('\n');
 }
