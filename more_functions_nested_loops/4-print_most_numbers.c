f243e22c18a3553caa15353c3c5f696b958a86d9
vicenzoanza <viceanza2010@hotmail.com>
2023-02-24 13:10:36 -0800

    task 5

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers - prints the numbers 0 to 9 but dont print 2 and 4, followed by a new line.
+**/
+void print_numbers(void)
+{
+int n = 48;
+
+while (n <= 57)
+{
+_putchar(n);
+n++;
+}
+_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,3 +3,5 @@ int _isupper(int c); /** 0 **/
 int _isdigit(int c); /** 1 **/
 int mul(int a, int b); /** 2 **/
 void print_numbers(void); /** 3 **/
+void print_most_numbers(void); /** 4 **/
+void more_numbers(void); /** 5 **/
