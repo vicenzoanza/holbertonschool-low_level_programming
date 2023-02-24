7797ca06edb703144c945b8338b31c1c530edebb
vicenzoanza <viceanza2010@hotmail.com>
2023-02-24 12:05:06 -0800

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - check the code
+*   *
+* Return: Always 0.
+**/
+void print_numbers(void);
+{
+int n;
+for (n > 0 ; n <= 9; n++)
+	printf("%d", n);
+	putchar('\n');
+return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _isupper(int c); /** 0 **/
 int _isdigit(int c); /** 1 **/
 int mul(int a, int b); /** 2 **/
+void print_numbers(void); /** 3 **/
