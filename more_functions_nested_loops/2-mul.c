ff87dd936eafb135b139c5550a273b9a32312e52
vicenzoanza <viceanza2010@hotmail.com>
2023-02-24 11:49:29 -0800

    task 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* mul - multiplies two integers.
+* @a: value to be multiplied.
+* @b: value to be multiplied.
+* Return: Always 0.
+**/
+int mul(int a, int b)
+{
+int mul = a * b;
+return (mul);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int _isupper(int c); /** 0 **/
 int _isdigit(int c); /** 1 **/
+int mul(int a, int b); /** 2 **/
