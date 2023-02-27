e6ee33ff6e5dd18e61ed9e7d9a111339376f3158
vicenzoanza <viceanza2010@hotmail.com>
2023-02-27 09:01:14 -0800

    task 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "stdio.h"
+#include "string.h"
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: string
+ **/
+int _strlen(char *s)
+{
+char c[16]="My first strlen";
+
+printf("%s\n", c);
+}
