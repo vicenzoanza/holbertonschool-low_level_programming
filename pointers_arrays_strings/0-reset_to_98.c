26cf37afc06f0dd0e75febeca4c11b3ded836454
vicenzoanza <viceanza2010@hotmail.com>
2023-02-27 06:47:45 -0800

    task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer and update the value 
+ * @n: Value
+ **/
+void reset_to_98(int *n)
+{
+int *n;
+*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+int _putchar(char); /** gen **/
+void reset_to_98(int *n); /** 0 **/
+void swap_int(int *a, int *b); /** 1 **/
+int _strlen(char *s); /** 2 **/
+void _puts(char *str); /** 3 **/
+void print_rev(char *s); /** 4 **/
+void rev_string(char *s); /** 5 **/
+void puts2(char *str); /** 6 **/
+void puts_half(char *str); /** 7 **/ 
+void print_array(int *a, int n); /** 8 **/  
+char *_strcpy(char *dest, char *src); /** 9 **/
+int _atoi(char *s); /** 10 **/
