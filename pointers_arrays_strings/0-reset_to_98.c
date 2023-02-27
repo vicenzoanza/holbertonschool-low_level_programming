dc5730ed01ae62080da2840fc5319ae5a4055668
vicenzoanza <viceanza2010@hotmail.com>
2023-02-27 06:50:02 -0800

    task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * reset_to_98 - takes a pointer and update the value 
- * @n: Value
- **/
+* reset_to_98 - takes a pointer and update the value
+* @n: Value
+**/
 void reset_to_98(int *n)
 {
 *n = 98;
