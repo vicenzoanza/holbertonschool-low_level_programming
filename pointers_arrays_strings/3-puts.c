045b234ed795c3fe33c336f178e04a823befb190
vicenzoanza <viceanza2010@hotmail.com>
2023-02-28 05:33:57 -0800

    task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints a string, followed by new line. 
+ * @str: string
+ **/
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
