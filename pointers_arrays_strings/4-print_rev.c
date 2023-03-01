cbd9b73d1d88c8b50954e73cc495433395d48af0
vicenzoanza <viceanza2010@hotmail.com>
2023-03-01 06:31:13 -0800

    task 3

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_rev - prints a string, in reverse, followed by new line.
+* @s: string
+**/
+void print_rev(char *s)
+{
+char *s[95]
+
+printf("\nI do not fear computers. I fear the lack of them - Isaac Asimov");
+scanf("%s", *s);
+printf("\n I do not fear computers. I fear the lack of them - Isaac Asimov:%s", strrev(*s));
+return (0);
+}
