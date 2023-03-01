ce55a4639c6e92c1de2974e0d0d459a559ea2141
vicenzoanza <viceanza2010@hotmail.com>
2023-03-01 08:38:55 -0800

    task 4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,11 @@
 **/
 void print_rev(char *s)
 {
-int length = strlen(s);
-    
-for (int i = length - 1; i >= 0; i--) {
-        printf("%c", s[i]);
-    }
-    printf("\n");
+int length = strlen;
+
+for (int i = length - 1; i >= 0; i--)
+{
+printf("%c", s[i]);
+}
+printf("\n");
 }
