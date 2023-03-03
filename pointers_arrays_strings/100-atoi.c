8b00646602175f038b24b3b00fb207b5a325fc06
vicenzoanza <viceanza2010@hotmail.com>
2023-03-03 06:47:39 -0800

    task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,24 +6,24 @@
  **/
 int _atoi(char *s)
 {
-    int results = 0;
-    int sign = 1;
-    int finish;
+int results = 0;
+int sign = 1;
+int finish;
 
-    while (*s != '\0' && (*s < '0' || *s > '9')) 
-    {
-        if (*s == '-') 
-	{
-            sign = -sign;
-        }
-        s++;
-    }
+while (*s != '\0' && (*s < '0' || *s > '9'))
+{
+if (*s == '-')
+{
+sign = -sign;
+}
+s++;
+}
 
-    while (*s >= '0' && *s <= '9') 
-    {
-        results = results * 10 + (*s - '0');
-        s++;
-    }
-finish = sign * results;
+while (*s >= '0' && *s <= '9')
+{
+results = results * 10 + (*s - '0');
+s++;
+}
+finish = (sign *results);
 return (finish);
 }
