e8a863092adc31c8ddaea93acb8db57ddd86fc06
vicenzoanza <viceanza2010@hotmail.com>
2023-03-03 03:34:51 -0800

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints elements in a array
+ * @a: array of string
+ * @n: number of elements
+ **/
+void print_array(int *a, int n)
+{
+int i = 0;
+
+for (; i < n; i++)
+{
+        printf("%d", a[i]);
+
+        if (i < n - 1)
+        {
+            printf(", ");
+        }
+    }
+printf("\n");
+}
