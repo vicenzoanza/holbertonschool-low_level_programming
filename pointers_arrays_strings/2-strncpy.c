5772a9a45dad28f41458501f9b6f1ccf4f80269d
vicenzoanza <viceanza2010@hotmail.com>
2023-03-05 11:53:05 -0800

    task 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy - function that copies a string.
+ * @dest: string copy destination.
+ * @src: the string to copy.
+ * @n: number of characters to copy.
+ * Return: dest.
+ **/
+char *_strncpy(char *dest, char *src, int n)
+{
+int p = 0;
+
+while (p < n && src[p] != '\0')
+{
+dest[p] = src[p];
+p++;
+}
+if (p < n)
+{
+dest[p] = '\0';
+p++;
+}
+return dest;
+}
