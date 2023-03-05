a8edf175469fc510cdb46b226c6af0a318e8aa49
vicenzoanza <viceanza2010@hotmail.com>
2023-03-05 10:21:17 -0800

    task 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - function that concatenates two strings.
+ * @dest: dest to string concatenate.
+ * @src: string to concatenate.
+ * Return: pointer |dest.
+ **/
+char *_strcat(char *dest, char *src)
+{
+char *n = dest;
+while (*n != '\0')
+{
+n++;
+}
+while (*src != '\0')
+{
+*n++ = *src++;
+}
+*n = '\0';
+return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,3 +10,11 @@ void puts_half(char *str); /** 7 **/
 void print_array(int *a, int n); /** 8 **/  
 char *_strcpy(char *dest, char *src); /** 9 **/
 int _atoi(char *s); /** 10 **/
+char *_strcat(char *dest, char *src); /** 0 **/
+char *_strncat(char *dest, char *src, int n); /** 1 **/
+char *_strncpy(char *dest, char *src, int n); /** 2 **/
+int _strcmp(char *s1, char *s2); /** 3 **/
+void reverse_array(int *a, int n); /** 4 **/
+char *string_toupper(char *); /** 5 **/
+char *cap_string(char *); /** 6 **/
+char *cap_string(char *); /** 7 **/
