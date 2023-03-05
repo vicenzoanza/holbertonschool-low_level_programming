9d0f8d3651952bbe837ff72a703d25f0b2d9d759
vicenzoanza <viceanza2010@hotmail.com>
2023-03-05 13:19:34 -0800

    task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - converts all lowercase letters in a string to uppercase.
+ * @str: string to modify.
+ * Return: modified string.
+ **/
+char *string_toupper(char *p)
+{
+int i = 0;
+
+for (; p[i] != '\0'; i++)
+{
+if (p[i] >= 'a' && p[i] <= 'z')
+{
+p[i] -= 32;
+}
+}
+return (p);
+}
