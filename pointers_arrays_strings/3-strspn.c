27112fe68ee9745808fec672f34e8f5606b91202
vicenzoanza <viceanza2010@hotmail.com>
2023-03-05 18:17:08 -0800

    task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/** 
+ * _strspn - get the length.
+ * @s: string.
+ * @accept: chart.
+ * Return: the number of bytes in the initial segment.
+ **/
+unsigned int _strspn(char *s, char *accept)
+{
+ unsigned int p = 0;
+ int j;
+ int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++) {
+if (s[i] == accept[j])
+{
+p++;
+break;
+}
+}
+if (accept[j] == '\0') 
+{
+break;
+}
+}
+return (p);
+}
