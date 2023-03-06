853acf6a7ac7637e2ce8bf04a33749e5ae27a4cc
vicenzoanza <viceanza2010@hotmail.com>
2023-03-05 16:53:17 -0800

    task 1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _mempcy - Copies n bytes from memory src to memory  dest.
+ * _memcpy - Copies n bytes from memory src to memory  dest.
  * @dest: destination memory
  * @src: source memory.
  * @n: bytes to copy.
@@ -8,7 +8,7 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i = 0;
-for (i = 0; i < n; i++) 
+for (i = 0; i < n; i++)
 {
 dest[i] = src[i];
 }
