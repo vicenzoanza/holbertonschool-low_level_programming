2bdb61488f661e7cc83f234c287d3a3cc46ed09e
vicenzoanza <viceanza2010@hotmail.com>
2023-03-06 08:35:12 -0800

    task 0

---
+++ recursion/main.h
@@ -0,0 +1,8 @@
+int _putchar(char); /** gen **/
+void _puts_recursion(char *s); /** 0 **/
+void _print_rev_recursion(char *s); /** 1 **/
+int _strlen_recursion(char *s); /** 2 **/
+int factorial(int n); /** 3 **/
+int _pow_recursion(int x, int y); /** 4 **/
+int _sqrt_recursion(int n); /** 5 **/
+int is_prime_number(int n); /** 6 **/
