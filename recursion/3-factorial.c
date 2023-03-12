012e7f89bbe55589fa499c28fd019764e3af281b
vicenzoanza <viceanza2010@hotmail.com>
2023-03-12 12:35:17 -0700

    task 0

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -4,18 +4,18 @@
  * @n: number to factorize
  * Return: -1 to indicate an error. 1 if 0 is factorized.
  **/
-int factorial(int n) 
+int factorial(int n)
 {
 int r = 1;
 int i = 1;
 
-if (n < 0) 
+if (n < 0)
 {
-return -1; // error: invalid input
+return (-1);
 }
 else if (n == 0)
 {
-return 1; // base case: 0! = 1
+return (1);
 }
 else
 {
