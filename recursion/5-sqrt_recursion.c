e5478f22834f696e1908f4550d674f635d9be196
vicenzoanza <viceanza2010@hotmail.com>
2023-03-12 13:14:01 -0700

    task 5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: number.
+ * Return: square root of the given number.
+ **/
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0 || n == 1);
+{
+return (n);
+}
+}
