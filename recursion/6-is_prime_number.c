10abbe9cceeca579296a777433de310e79043f0f
vicenzoanza <viceanza2010@hotmail.com>
2023-03-12 14:07:24 -0700

    task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,32 +4,31 @@
  * or not.
  * @n: number
  * Return: 1 if the number is a prime number. 0 if not.
- */
+ **/
 int is_prime_number(int n)
 {
-int i;
-
 if (n <= 1)
 {
-return (0);
+return 0;
 }
-else if (n == 2 || n == 3)
+return is_prime_helper(n, 2);
+}
+/**
+ * is_prime_helper - recursive helper function for is_prime_number.
+ * @n: number to check.
+ * @i: current divisor.
+ * Return: 1 if n is prime, 0 if not.
+ **/
+int is_prime_helper(int n, int i)
+{
+if (i == n)
 {
 return (1);
 }
-else if (n % 2 == 0)
-{
-return (0);
-}
-else
-{
-for (i = 3; i <= n / 2; i += 2)
-{
 if (n % i == 0)
 {
 return (0);
 }
+return is_prime_helper(n, i + 1);
 }
-return (1);
-}
-}
+
