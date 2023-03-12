0d2ac37c06cf6a45694e341431f6b82902f48582
vicenzoanza <viceanza2010@hotmail.com>
2023-03-12 14:40:34 -0700

    task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints its name, followed by a new line.
+ * @argc: argument count.
+ * @argv: argument vector.
+ * Return: Always.
+ **/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+(void)argc;
+return (0);
+}
