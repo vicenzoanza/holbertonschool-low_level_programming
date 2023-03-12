0d9264d0989dc0d250100a4701e689f58e5f781e
vicenzoanza <viceanza2010@hotmail.com>
2023-03-12 13:52:23 -0700

    task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -11,12 +11,23 @@ if (n <= 1)
 {
 return (0);
 }
-if (n <= 3)
-{	
+else if (n == 3 || n == 2)
+{
 return (1);
 }
-if (n % 2 == 0)
+else if (n % 2 == 0)
 {
 return (0);
+}
+else
+{
+for (int i = 3; i <= n / 2; i += 2)
+{
+if (n % i == 0)
+{
+return (0);
+}
+}
+return (1);
 }
 }
