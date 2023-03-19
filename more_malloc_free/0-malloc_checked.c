c7cab082ee3e31d0fc4cc2582ea13b18c99ffae0
vicenzoanza <viceanza2010@hotmail.com>
2023-03-19 13:19:13 -0700

    task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - allocates memory using.
+ * @b: memory size.
+ * Return: pointer to the allocated memory.
+ **/ 
+void *malloc_checked(unsigned int b)
+{
+void *ppp = malloc(b);
+
+if (ppp == NULL)
+{
+exit(98);
+}
+return (ppp);
+}
---
+++ more_malloc_free/main.h
@@ -0,0 +1,4 @@
+void *malloc_checked(unsigned int b); /** 0 **/
+char *string_nconcat(char *s1, char *s2, unsigned int n); /** 1 **/
+void *_calloc(unsigned int nmemb, unsigned int size); /** 2 **/
+int *array_range(int min, int max); /** 3 **/
