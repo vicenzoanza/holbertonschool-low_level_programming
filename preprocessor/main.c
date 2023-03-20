023c9ef00dbd919689b8a026ba3e51a34cfaf95c
vicenzoanza <viceanza2010@hotmail.com>
2023-03-19 17:29:39 -0700

    task 4

--- preprocessor/main.c
+++ preprocessor/main.c
@@ -1,6 +1,6 @@
-#include "1-pi.h"
-#include "1-pi.h"
 #include <stdio.h>
+#include "4-sum.h"
+#include "4-sum.h"
 
 /**
  * main - check the code
@@ -9,12 +9,9 @@
  */
 int main(void)
 {
-    float a;
-    float r;
+    int s;
 
-    r = 98;
-    a = PI * r * r;
-    printf("%.3f\n", a);
+    s = SUM(98, 1024);
+    printf("%d\n", s);
     return (0);
 }
-
