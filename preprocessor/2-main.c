c5be105b065e008dd7b3d601b24a05a8900d1709
vicenzoanza <viceanza2010@hotmail.com>
2023-03-19 18:23:21 -0700

    task 2

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Prints the name of the file it was compiled. 
+ * main - Prints the name of the file it was compiled.
  * followed by a new line
  * Return: always 0 (success)
  **/
 
 int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
