91ac28c2811fe7f9ed3edbdaf2b931b4033a51f5
vicenzoanza <viceanza2010@hotmail.com>
2023-03-25 23:56:37 -0700

    task 0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,14 @@
+#ifndef dog_h
+#define dog_h
 /**
  * struct dog - structure of a dog.
  * @name: name.
  * @age: age.
  * @owner: owner.
- **/ 
+ **/
 struct dog
 {
-char* name;
+char *name;
 float age;
-char* owner;
+char *owner;
 };
