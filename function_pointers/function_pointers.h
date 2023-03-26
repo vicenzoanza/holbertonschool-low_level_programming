682e1f6bb073f6e2869a19aef242b07ae86af14b
vicenzoanza <viceanza2010@hotmail.com>
2023-03-26 00:26:22 -0700

    task 0

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef main_h
 #define main_h
 #include <stddef.h>
-void print_name(char *name, void (*f)(char *)); // 0
-void array_iterator(int *array, size_t size, void (*action)(int)); // 1
-int int_index(int *array, int size, int (*cmp)(int)); // 2
+void print_name(char *name, void (*f)(char *)); /** 0 **/
+void array_iterator(int *array, size_t size, void (*action)(int)); /** 1 **/
+int int_index(int *array, int size, int (*cmp)(int)); /** 2 **/
 #endif
