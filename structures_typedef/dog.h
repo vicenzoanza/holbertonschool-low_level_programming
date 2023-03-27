de995932adfef35621beeab00641ce453ec42314
vicenzoanza <viceanza2010@hotmail.com>
2023-03-26 19:28:36 -0700

    task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -11,7 +11,10 @@ struct dog
 char *name;
 float age;
 char *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner); /** 1 **/
 void print_dog(struct dog *d); /** 2 **/
+dog_t *new_dog(char *name, float age, char *owner); /** 3 **/
+void free_dog(dog_t *d); /** 4 **/
 #endif
