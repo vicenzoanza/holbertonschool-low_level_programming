8778505bfee0d5d0f1fc48e496ff29d883a316e8
vicenzoanza <viceanza2010@hotmail.com>
2023-03-26 18:38:35 -0700

    task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -10,9 +10,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list ppp;
 va_start(ppp, n);
-unsigned int i = 0;
 
-for (; i < n; i++)
+for (unsigned int i = 0; i < n; i++)
 {
 printf("%d", va_arg(ppp, int));
 if (separator != NULL && i < n - 1)
