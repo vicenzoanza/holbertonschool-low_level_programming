d61c8be312d050d42674be29e9424475151fdea5
vicenzoanza <viceanza2010@hotmail.com>
2023-03-26 19:45:20 -0700

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,8 +14,10 @@ dog_t *new_dog;
 char *name_copy, *owner_copy;
 int i, name_len = 0, owner_len = 0;
 
-while (name[name_len++]);
-while (owner[owner_len++]);
+while (name[name_len++])
+	;
+while (owner[owner_len++])
+	;
 new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
