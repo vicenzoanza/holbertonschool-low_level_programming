1e346c6328f950e47011fc2d52124b4cfcf2fe66
vicenzoanza <viceanza2010@hotmail.com>
2023-03-26 19:10:59 -0700

    task 1

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ *
+ *
+ **/
+void print_dog(struct dog *d)
+{
+if (d != NULL)
+{
+if (d->name == NULL)
+d->name = "(nil)";
+if (d->owner == NULL)
+d->owner = "(nil)";
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
+else
+return;
+}
