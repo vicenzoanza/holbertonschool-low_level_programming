1e20e86bcf88d196c0a54282ff85d48c85b33fab
vicenzoanza <viceanza2010@hotmail.com>
2023-03-26 19:01:42 -0700

    task 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,3 +1,4 @@
 int sum_them_all(const unsigned int n, ...); /** 0 **/
 void print_numbers(const char *separator, const unsigned int n, ...); /** 1 **/
 void print_strings(const char *separator, const unsigned int n, ...); /** 2 **/
+void print_all(const char * const format, ...); /** 3 **/
