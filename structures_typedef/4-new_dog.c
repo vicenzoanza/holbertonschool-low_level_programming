47db8a0e74618e0bc9e73e6ad1b5dd81775375e7
vicenzoanza <viceanza2010@hotmail.com>
2023-03-26 19:44:32 -0700

    task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog_t structure.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
+ * Return: pointer to the new dog_t structure or NULL if it fails.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *new_dog;
+char *name_copy, *owner_copy;
+int i, name_len = 0, owner_len = 0;
+
+while (name[name_len++]);
+while (owner[owner_len++]);
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
+
+name_copy = malloc(sizeof(char) * name_len);
+if (name_copy == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+owner_copy = malloc(sizeof(char) * owner_len);
+if (owner_copy == NULL)
+{
+free(name_copy);
+free(new_dog);
+return (NULL);
+}
+for (i = 0; i < name_len; i++)
+name_copy[i] = name[i];
+for (i = 0; i < owner_len; i++)
+owner_copy[i] = owner[i];
+
+new_dog->name = name_copy;
+new_dog->age = age;
+new_dog->owner = owner_copy;
+return (new_dog);
+}
---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function that frees dogs.
+ * @d: pointer to the dog.
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
