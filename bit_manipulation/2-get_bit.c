35824b11f90d804ad3d9aa07a3a682c6c32d1bce
vicenzoanza <viceanza2010@hotmail.com>
2023-04-10 17:53:37 -0700

    task 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
  * get_bit -  returns the value of a bit at a given index.
  * @n:number to convert
  * @index: number of the bit.
- * return: the value of the bit at index index or -1 if an error occured.
+ * Return: the value of the bit at index index or -1 if an error occured.
  **/
 int get_bit(unsigned long int n, unsigned int index)
 {
