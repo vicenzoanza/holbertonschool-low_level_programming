7f1fb45b8f05388c12291ea07f4f9f48471b95b3
vicenzoanza <viceanza2010@hotmail.com>
2023-04-11 11:34:34 -0700

    task6

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,4 +4,5 @@ void print_alphabet_x10(void); /** 2 **/
 int _islower(int c); /** 3 **/
 int _isalpha(int c); /** 4 **/
 int print_sign(int n);/** 5 **/
+int _abs(int); /** 6 **/
 int add(int, int); /** 10 **/
