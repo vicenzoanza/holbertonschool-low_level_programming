14c66e5db748344310ed83f81e5305449cbabc46
vicenzoanza <viceanza2010@hotmail.com>
2023-04-15 15:11:38 -0700

    task8

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,4 +6,5 @@ int _isalpha(int c); /** 4 **/
 int print_sign(int n);/** 5 **/
 int _abs(int); /** 6 **/
 int print_last_digit(int n); /** 7 **/
+void jack_bauer(void); /** 8 **/
 int add(int, int); /** 10 **/
