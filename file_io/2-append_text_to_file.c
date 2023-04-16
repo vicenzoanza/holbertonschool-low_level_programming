af85271483230de96c948e9c3b84c0c46ab21e47
vicenzoanza <viceanza2010@hotmail.com>
2023-04-16 02:16:05 -0700

    task 2

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -10,15 +10,15 @@ int append_text_to_file(const char *filename, char *text_content)
 int n, bt = 0;
 
 if (!filename)
-return -1;
+return (-1);
 
 n = open(filename, O_WRONLY | O_APPEND);
 if (n == -1)
-return -1;
+return (-1);
 
 if (text_content)
 bt = write(n, text_content, strlen(text_content));
 
 close(n);
-return (bt == -1) ? -1 : 1;
+return ((bt == -1) ? -1 : 1);
 }
