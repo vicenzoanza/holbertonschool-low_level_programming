254133dba3bdffa2f2d4b21ede8f85dde75173bf
vicenzoanza <viceanza2010@hotmail.com>
2023-04-16 21:16:45 -0700

    task 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -3,12 +3,11 @@
  * key_index - gives you the index of a key.
  * @key: key.
  * @size: size.
- * Return: index at which the key/value pair should be stored in the 
+ * Return: index at which the key/value pair should be stored in the
  * array of the hash table.
  **/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
- unsigned long int mx = hash_djb2(key);
- 
- return (mx % size);
+unsigned long int mx = hash_djb2(key);
+return (mx % size);
 }
