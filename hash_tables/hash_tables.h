2c5e39c73302a24e979dd51fe9e3c016ba00c9c7
vicenzoanza <viceanza2010@hotmail.com>
2023-04-16 20:58:29 -0700

    betty

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -40,9 +40,9 @@ typedef struct hash_table_s
 /* prototypes */
 
 hash_table_t *hash_table_create(unsigned long int size); /** 0 **/
-unsigned long int hash_djb2(const unsigned char *str); /** 1 **/ 
-unsigned long int key_index(const unsigned char *key, unsigned long int size); /** 2 **/
-int hash_table_set(hash_table_t *ht, const char *key, const char *value); /** 3 **/
+unsigned long int hash_djb2(const unsigned char *str); /** 1 **/
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key); /** 4 **/
 void hash_table_print(const hash_table_t *ht); /** 5 **/
 void hash_table_delete(hash_table_t *ht); /** 6 **/
