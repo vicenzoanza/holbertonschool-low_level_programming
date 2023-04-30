3a2904b8c38592b4731071cd793c07cdd9a4686e
vicenzoanza <viceanza2010@hotmail.com>
2023-04-30 13:48:24 -0700

    betty

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -3,7 +3,7 @@
  * hash_table_get - retrieves a value associated with a key.
  * @ht: hash table.
  * @key: key.
- * Return:  value associated with the element, or NULL if key couldn’t be found.
+ * Return: value associated with the element, NULL if key couldn’t be found.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
