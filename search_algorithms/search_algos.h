e0c2c4a5862306c4e0bfd1b62e69d619f088d835
vicenzoanza <viceanza2010@hotmail.com>
2023-05-19 14:00:37 -0700

    task 0

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -4,7 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int linear_search(int *array, size_t size, int value); /** 0 **/
+/**
+ * linear_search - Searches for a value in an array of integers using
+ * the Linear search algorithm
+ * @array: array
+ * @size: size
+ * @value: value
+ * Return: the first index where value is located.
+ **/
+int linear_search(int *array, size_t size, int value);
 
 
 
